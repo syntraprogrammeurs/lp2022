27af8a4f5c7e64a2f1195d76fc7a8ed678703695
Neverlanders <vanhoutte.tom@gmail.com>
2022-09-15 15:56:38 +0200

    oef1210

--- oefeningen/oefening1210/index.js
+++ oefeningen/oefening1210/index.js
@@ -1,22 +1,105 @@
 /*
-Schrijf een programma die alle priemgetallen in een rij afdrukt. 
-Voorbeeld:  Vraag een eindgetal aan de gebruiker: 
-Alle getallen gelijk aan en lager dan het eindgetal dienen te worden getest.
-  Zijn ze een priemgetal of niet. 
-  Een priemgetal is enkel deelbaar door 1 en zichzelf.
+1x1=1; 1x2=2; 1x3=3 ... 1x12=12
 */
-let eindGetal = Number(prompt("Geef een eindgetal in:"));
+
+/** stap 1 ***/
+/*const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
 
 for(let i=1;i<=eindGetal;i++){
-    let count=0;
-    for(let x=1;x<=i;x++){
-        if(i%x===0){
-            count++
+  console.log(tafel,"x",i,"=",tafel*i,",");
+}*/
+/**stap 2
+ * naast elkaar zetten van de berekeningen
+ * **/
+/*
+const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="";
+for(let i=1;i<=eindGetal;i++){
+    resultaat = resultaat + tafel+"x"+i+"="+tafel*i+","
+}
+console.log(resultaat);*/
+/**stap 3
+ * onder elkaar zetten per drie
+ * **/
+/*const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="";
+for(let i=1;i<=eindGetal;i++){
+    for(let x=1;x<=3;x++){
+        resultaat = resultaat + tafel+"x"+i+"="+tafel*i+","
+    }
+    console.log(resultaat);
+    resultaat ="";
+}*/
+/**stap 4
+ * extra teller voorzien om te vermenigvuldigen
+ * de i wordt vervangen door een andere teller
+ * **/
+/*const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="";
+let y=1;
+for(let i=1;i<=eindGetal;i++){
+    for(let x=1;x<=3;x++){
+        resultaat = resultaat + tafel+"x"+y+"="+tafel*y+","
+        y++;
+    }
+    console.log(resultaat);
+    resultaat ="";
+}*/
+/**stap 5
+ * enkel het aantal lijnen tonen die gevraagd zijn
+ * **/
+/*const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="";
+let y=1;
+for(let i=1;i<=eindGetal;i++){
+    if(y<=eindGetal){
+        for(let x=1;x<=3;x++){
+            resultaat = resultaat + tafel+"x"+y+"="+tafel*y+","
+            y++;
         }
     }
-    if(count == 2){
-        console.log(i);
+    console.log(resultaat);
+    resultaat ="";
+}*/
+/**stap 6
+ * komma's weg
+ * **/
+/*const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="";
+let y=1;
+for(let i=1;i<=eindGetal;i++){
+    if(y<=eindGetal){
+        for(let x=1;x<=3;x++){
+            resultaat = resultaat + tafel+"x"+y+"="+tafel*y+","
+            y++;
+        }
     }
+    console.log(resultaat.substring(0,resultaat.length-1));
+    resultaat ="";
+}*/
+/**stap 7
+ * test op tafel 2 met combinatie 49 verwijder overbodige berekening
+ * **/
+const tafel = Number(prompt("geef een tafel in:")); //1
+const eindGetal = Number(prompt("Geef een eindgetal in:"));//12
+let resultaat="■ ";
+let y=1;
+for(let i=1;i<=eindGetal;i++){
+    if(y<=eindGetal){
+        for(let x=1;x<=3;x++){
+            if(y<=eindGetal) {
+                resultaat = resultaat + tafel + "x" + y + "=" + tafel * y + ","
+                y++;
+            }
+        }
+        console.log(resultaat.substring(0,resultaat.length-1));
+        resultaat ="■ ";
+    }
+
 }
-
-
