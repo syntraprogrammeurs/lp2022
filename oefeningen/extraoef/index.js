f8fba58cbe7e8a4f35a71f00d69bf9eac07880ca
Neverlanders <vanhoutte.tom@gmail.com>
2022-09-15 12:17:49 +0200

    extra oef

---
+++ oefeningen/extraoef/index.js
@@ -0,0 +1,9 @@
+/**
+ * Geef een te raden getal in: 67
+ * Raad het getal: 5
+ *
+ * de pc zegt dan hoger of lager tot het getal geraden is
+ * geef ook weer hoeveel pogingen de gebruiker nodig heeft.
+ *
+ * for loop!
+ * **/
--- oefeningen/oefening129/index.js
+++ oefeningen/oefening129/index.js
@@ -1,22 +1,4 @@
-/*
-Schrijf een programma die alle priemgetallen in een rij afdrukt. 
-Voorbeeld:  Vraag een eindgetal aan de gebruiker: 
-Alle getallen gelijk aan en lager dan het eindgetal dienen te worden getest.
-  Zijn ze een priemgetal of niet. 
-  Een priemgetal is enkel deelbaar door 1 en zichzelf.
-*/
-let eindGetal = Number(prompt("Geef een eindgetal in:"));
 
-for(let i=1;i<=eindGetal;i++){
-    let count=0;
-    for(let x=1;x<=i;x++){
-        if(i%x===0){
-            count++
-        }
-    }
-    if(count == 2){
-        console.log(i);
-    }
-}
+
 
 
