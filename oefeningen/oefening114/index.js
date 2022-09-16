d008cd8bea360593c137231dac2602314b86f3da
Tom <syntraprogrammeurs@gmail.com>
2022-09-16 11:58:04 +0200

    oef114

--- oefeningen/oefening114/index.js
+++ oefeningen/oefening114/index.js
@@ -2,6 +2,9 @@ let geboorteJaar = Number(prompt("Geef je geboorteJaar in"));
 let lopendeJaar = Number(prompt("Geef het huidige jaartal in"));
 if(lopendeJaar - geboorteJaar >= 18){
     console.log("Vanaf nu mag, kan en beslis ik alles, binnen de wettelijke grenzen.")
-}else {
+}else if(lopendeJaar- geboorteJaar < 1){
+    console.log("Geboortejaar is groter dan huidig jaar, dit kan niet")
+}else{
     console.log("Gelukkig heb ik mijn ouders die alles voor me regelen.")
 }
+
