790cfbef3d9e3836ed7b67be1ad2c72003e9a671
Tom <syntraprogrammeurs@gmail.com>
2022-09-16 08:45:22 +0200

    oef1214

---
+++ oefeningen/oefening1214/index.js
@@ -0,0 +1,12 @@
+/*
+Schrijf een programma die een aantal getallen inleest en
+eindigt wanneer de gebruiker een negatief getal ingeeft.
+ Schrijf daarna de som van de getallen op het scherm.
+*/
+let som = 0;
+let getal = Number(prompt('Geef een getal in, stop met een negatief getal'));
+while(getal >0){
+    som += getal;
+    getal = Number(prompt('Geef een getal in, stop met een negatief getal'));
+}
+console.log(som);
